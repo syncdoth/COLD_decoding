f488c994c87de460243b66e3edf73fb75d8724a4
syncdoth <choisehyun98@gmail.com>
2022-10-20 16:00:20 +0800

    attr_control script to experiment over constraint weight

--- scripts/sst2_attr_control.sh
+++ scripts/sst2_attr_control.sh
@@ -5,16 +5,17 @@ function control_gen() {
 	prompt=$1
 	exp_name=$2
 	class_idx=$3
+	cw=$4
 	script="python3 cold_decoding.py \
 		--seed 12 \
 		--mode prompted_generation \
-		--pretrained_model gpt2-xl  \
+		--pretrained_model gpt2-xl \
 		--init-temp 1 \
 		--length 20 \
 		--max-length 20 \
 		--num-iters 2000 \
 		--min-iters 1000 \
-		--constraint-weight 0.5 \
+		--constraint-weight $cw \
 		--attr_control_weight 1.0 \
 		--stepsize 0.1 \
 		--noise-iters 1 \
@@ -29,7 +30,7 @@ function control_gen() {
 		--large_gs_std 1,0.5,0.1,0.05  \
 		--stepsize-ratio 1  \
 		--batch-size 5 \
-		--repeat-batch 10 \
+		--repeat-batch 1 \
 		--print-every 200 \
 		--output-dir './outputs/attr_control/' \
 		--output-filename '${exp_name}.json' \
@@ -43,9 +44,10 @@ function control_gen() {
 		--wandb-runname $exp_name"
 	eval $script
 }
+for cw in 0.2 0.4 0.5 0.8 1.0; do
+	# positive
+	control_gen "The food at the restaurant was" "gpt2-xl-ctrl_sst2_pos-pool_max-cw$cw" 1 $cw &
 
-# positive
-control_gen "The food at the restaurant was" "gpt2-xl-ctrl_sst2_pos-pool_max" 1
-
-# negative
-control_gen "The food at the restaurant was" "gpt2-xl-ctrl_sst2_neg-pool_max" 0
+	# negative
+	control_gen "The food at the restaurant was" "gpt2-xl-ctrl_sst2_neg-pool_max-cw$cw" 0 $cw
+done
