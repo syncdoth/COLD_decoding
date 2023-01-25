b28c694ff1cbce980ac36698daca8bd79833c25f
syncdoth <choisehyun98@gmail.com>
2023-01-25 13:36:16 +0800

    check notebook or not util

--- util.py
+++ util.py
@@ -20,6 +20,17 @@ from nltk.corpus import stopwords
 from bleuloss import batch_log_bleulosscnn_ae
 
 
+def in_notebook():
+    try:
+        from IPython import get_ipython
+        if 'IPKernelApp' not in get_ipython().config:  # pragma: no cover
+            return False
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    return True
+
 def set_random_seeds(seed):
     """
     set the random seed of all related libraries
